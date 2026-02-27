62d432e587653642b91cd13ecd473eb2e76f197d
Joher Franco <joherstivenfrancoa@gmail.com>
2026-02-27 15:21:54 -0500

    Actualizo

--- suma.py/hotel.py
+++ suma.py/hotel.py
@@ -1,5 +1,13 @@
 clientes = []
 
+habitaciones =[
+        {"estado": "Disponible", "numero": 101, "tipo" : "sencilla", "precio" : 100000},
+        {"estado": "Disponible", "numero": 102, "tipo" : "doble", "precio" : 150000},
+        {"estado": "Disponible", "numero": 103, "tipo" : "sencilla", "precio" : 100000},
+        {"estado": "Ocupada", "numero": 104, "tipo" : "doble", "precio" : 150000}
+    ]
+
+
 def pedir_int(mensaje):
     while True:
         try:
@@ -27,17 +35,46 @@ def crear_cliente():
 
 def habitaciones_disponibles():
 
-    for habitacion in habitaciones_disponibles:
+    hay_disponibles = False
+
+    for habitacion in habitaciones:
         if habitacion["estado"] == "Disponible":
             print(f"Número: {habitacion['numero']}")
             print(f"Tipo: {habitacion['tipo']}")
             print(f"Precio: {habitacion['precio']}")
             print("---------------------")
 
+    if not hay_disponibles:
+        print("No hay habitaciones Disponibles")
 
-def cancelar_reservacion():
-    for habitacion in habitaciones_disponibles:
-        if habitacion 
+
+
+def menu():
+    while True:
+        print("==BIENVENIDO==")
+        print("1.Ingrese sus datos: ")
+        print("2. Habitaciones Disponibles: ")
+        print("3. Salir del Programa: ")
+
+        opcion = input("Selecciona una Opcion: ")
+
+        if opcion == "1":
+            crear_cliente()
+        elif opcion == "2":
+            habitaciones_disponibles()
+        if opcion == "3":
+            "Saliendo del programa"
+            break
+        else:
+            print("por favor ingresa una opccion valida")
+
+menu()
+
+
+        
+
+
+
 
         
 
