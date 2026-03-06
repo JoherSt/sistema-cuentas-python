5d01b98ba6b87728975ec2703f67deba4cf74b7c
Joher Franco <joherstivenfrancoa@gmail.com>
2026-03-06 16:46:07 -0500

    Actualizo

--- Trabajos.py/calculadora.py
+++ Trabajos.py/calculadora.py
@@ -1,21 +1,14 @@
-suma = "resultado"
+num1 = int(input("Ingresa un Numero: "))
+num2 = int(input("Ingrese un Numero: "))
 
-num1 =int(input("ingresa un numero: "))
-num2 =int(input("ingresa otro numero por Favor: "))
-
-operacion = (input("Ingrese la operecion que quieras hacer (+ * - /)  "))
+operacion = input("Elija la Operacion que deseas Realizar(+, -, *, /)")
 
 if operacion == "+":
-    print(f"la suma es: {num1 + num2} ")
-elif operacion == "*":
-    print (f"la  multiplicacion es: {num1 * num2}")
+    print(f"La Resultado es: {num1 + num2}")
 elif operacion == "-":
-    print(f"La resta es: {num1 - num2}")
+    print(f"El Resultado es: {num1 - num2}")
+elif operacion == "*":
+    print(f"El Resultado es: {num1 * num2}")
 elif operacion == "/":
-    print(f"La division es: {num1 / num2}")
-else:
-    print("ingresa una Operacion Valida")
-
-
-
-
+    print(f"El Resultado es: {num1 / num2}")
+    
